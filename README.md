46bbbdc41558914d2f741885efead008684ccd62
ManuelKy08 <88957055+ManuelKy08@users.noreply.github.com>
2024-09-12 06:56:29 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dork Engine
+
+Dork/Google Dork Sama dengan "https://www.exploit-db.com/google-hacking-database"
+
+## Screenshot
+
+![dork engine](<a href="https://freeimage.host/i/dUmrJg1"><img src="https://iili.io/dUmrJg1.md.png" alt="dUmrJg1.md.png" border="0"></a>)
---
+++ src/script.js
@@ -0,0 +1,15 @@
+function runDork(dork) {
+    const domain = document.getElementById('domainInput').value;
+    if (domain) {
+        const finalDork = dork.replace('example.com', domain);
+        
+        if (finalDork.startsWith('http://') || finalDork.startsWith('https://')) {
+            window.open(finalDork, '_blank');
+        } else {
+
+            window.open(`https://www.google.com/search?q=${encodeURIComponent(finalDork)}`, '_blank');
+        }
+    } else {
+        alert('Please enter a target domain.');
+    }
+}
